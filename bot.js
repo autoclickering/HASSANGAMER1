d1b58ce53d49d1a73bbb303db4f3553ba7a5696b
autoclickering <44882252+autoclickering@users.noreply.github.com>
2018-11-08 14:40:49 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,21 @@
-﻿const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+client.on('ready', () => {
+    console.log(`~~~~~~~~~~~~~~~~~`);
+    console.log(`Logging into Discord`);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`on  ${client.guilds.size} Servers `);
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(`سبام `,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("online")
+ });
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("498233231922036756")
+let channel =     client.channels.get("504998912860618758")
 setInterval(function() {
-channel.send(`youtube`);
+channel.send(`الله اكبر♥`);
 }, 25)
 })
  
